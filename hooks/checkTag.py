173533988fb16e6bf256a709662cd732d1e97a13
Ryokohbato <ryokohbato@gmail.com>
2020-11-04 17:52:35 +0900

    [update] Output test results with color.

--- hooks/checkTag.py
+++ hooks/checkTag.py
@@ -1,14 +1,23 @@
 from subprocess import getoutput
 import re
 
+class ConsoleColor:
+  GREEN = '\033[32m'
+  YELLOW = '\033[33m'
+  END = '\033[0m'
+
+print('\n')
+
 # Check the versions written in json files.
 srcVer = getoutput('npm run checkVersion')
 
 for line in srcVer.split('\n'):
   matchResult =re.match(r'^.+\.json.*$', line)
   if (matchResult is not None and len(matchResult.group()) > 0):
-    print(line)
+    print(ConsoleColor.YELLOW + line + ConsoleColor.END)
 
 # Display the latest git tag.
 allTag = getoutput('git tag').split('\n')
-print("Latest release: " + allTag[len(allTag) - 1])
+print(ConsoleColor.GREEN + "Latest release: " + allTag[len(allTag) - 1] + ConsoleColor.END)
+
+print('\n')

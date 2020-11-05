a12108fc7350f81d5a8c97e2cc5530faae41cbc4
Ryokohbato <ryokohbato@gmail.com>
2020-11-05 19:11:28 +0900

    [add] Add script to make icon images.

---
+++ src/images/makeIcons.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+convert -resize 48x48 icon_128.png icon_48.png
+convert -resize 32x32 icon_128.png icon_32.png
+convert -resize 24x24 icon_128.png icon_24.png
+convert -resize 16x16 icon_128.png icon_16.png

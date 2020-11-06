7dd8be465c0561b0f0eaeefcf32ff6444c5156ec
Ryokohbato <ryokohbato@gmail.com>
2020-11-06 21:48:53 +0900

    [update] Improve accessibility.

--- src/script/modules/ToggleActive.ts
+++ src/script/modules/ToggleActive.ts
@@ -12,21 +12,39 @@ export const toggleActive = function(): void
     {
       x.addEventListener('click', () =>
       {
-        if (!x.classList.contains('is-active'))
-        {
-          const triggerTag = x.getAttribute('trigger-tag') as string;
-          const sameTag: NodeListOf<Element>|null = document.querySelectorAll(triggerTag);
+        toggleActive__exec(x);
+      })
 
-          document.querySelector(`.is-active[is-trigger][trigger-tag="${x.getAttribute('trigger-tag')}"]`)?.classList.remove('is-active');
-          sameTag.forEach(y =>
-            {
-              if (y.classList.contains('is-active'))
-                y.classList.remove('is-active');
-            })
-
-          x.classList.add('is-active');
-          document.querySelector(`[trigger-target-tag="${x.getAttribute('trigger-tag')}__${x.getAttribute('trigger-for')}"]`)?.classList.add('is-active');
-        }
+      x.addEventListener('keydown', (event: KeyboardEvent) =>
+      {
+        if (event.isComposing || event.key === 'Enter')
+          toggleActive__exec(x);
       })
     })
+}
+
+const toggleActive__exec = (x: Element): void =>
+{
+  if (!x.classList.contains('is-active'))
+  {
+    const triggerTag = x.getAttribute('trigger-tag') as string;
+    const sameTag: NodeListOf<Element>|null = document.querySelectorAll(triggerTag);
+
+    document.querySelector(`.is-active[is-trigger][trigger-tag="${x.getAttribute('trigger-tag')}"]`)?.setAttribute('aria-selected', 'false');
+    document.querySelector(`.is-active[is-trigger][trigger-tag="${x.getAttribute('trigger-tag')}"]`)?.classList.remove('is-active');
+    sameTag.forEach(y =>
+      {
+        if (y.classList.contains('is-active'))
+          y.classList.remove('is-active');
+
+        if (y.hasAttribute('aria-hidden'))
+          y.setAttribute('aria-hidden', 'true');
+      })
+
+    x.classList.add('is-active');
+    x.setAttribute('aria-selected', 'true');
+    console.log(x)
+    document.querySelector(`[trigger-target-tag="${x.getAttribute('trigger-tag')}__${x.getAttribute('trigger-for')}"]`)?.setAttribute('aria-hidden', 'false');
+    document.querySelector(`[trigger-target-tag="${x.getAttribute('trigger-tag')}__${x.getAttribute('trigger-for')}"]`)?.classList.add('is-active');
+  }
 }

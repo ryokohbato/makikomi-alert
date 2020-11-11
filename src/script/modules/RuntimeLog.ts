eddf4095d8e8b6eb136d8fcb68c105af0b3c22be
Ryokohbato <ryokohbato@gmail.com>
2020-11-11 19:03:44 +0900

    [update] Static RuntimeLog

--- src/script/modules/RuntimeLog.ts
+++ src/script/modules/RuntimeLog.ts
@@ -2,16 +2,25 @@ import { IRuntimeInformation } from "./RuntimeInformationInterface";
 
 export class RuntimeLog
 {
-  private static runtimeLog: Array<IRuntimeInformation>;
-  
-  constructor()
-  {
-    RuntimeLog.runtimeLog = [];
-  }
+  private static runtimeLog: Array<IRuntimeInformation> = [];
 
-  public Add(log: IRuntimeInformation): Array<IRuntimeInformation>
+  public static Add(log: IRuntimeInformation): Array<IRuntimeInformation>
   {
     RuntimeLog.runtimeLog = [...RuntimeLog.runtimeLog, log];
+
+    if (this.GetLogCount() > 150)
+      RuntimeLog.runtimeLog = this._GetRecent();
+
     return RuntimeLog.runtimeLog;
+  }
+
+  public static GetLogCount()
+  {
+    return RuntimeLog.runtimeLog ? RuntimeLog.runtimeLog?.length : 0;
+  }
+
+  private static _GetRecent(): Array<IRuntimeInformation>
+  {
+    return RuntimeLog.runtimeLog.filter((value, index, array) => index > array.length - 100)
   }
 }
